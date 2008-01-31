6ede98adddf45cf4b4234325ade46b1d70a0303c
tsmr <tsmr@349b9182-4a13-0410-896f-e5e9767dd1b3>
2008-01-31 23:59:08 +0000

    [cacti - pdf] traduction hu_HU.php Thanks
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@40 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/hu_HU.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: BALPE Dévi
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+$LANGPDF["title"][1]="Nyomtatás PDF-be";
+$LANGPDF["title"][2]="Válaszdki a pdf-be nyomtatandó táblákat";
+
+$LANGPDF["button"][1]="Nyomtatás";
+$LANGPDF["button"][2]="Mentés";
+
+$LANGPDF["config"][1]="PDF modul beállítása";
+$LANGPDF["config"][2]="PDF modul telepítése";
+$LANGPDF["config"][3]="PDF modul eltávolítása";
+$LANGPDF["config"][4]="Beállítások";
+$LANGPDF["config"][5]="Leltári tárgy típusa :";
+$LANGPDF["config"][6]="Profilok jogosultságai";
+$LANGPDF["config"][7]="Jogok a következő profilhoz";
+
+$LANGPDF["note"][1]="Nincs megjegyzés";
+
+$LANGPDF["document"][1]="Nincs hozzárendelt dokumentum";
+
+$LANGPDF["financial"][1]="Nincs pénzügyi információ";
+$LANGPDF["financial"][2]="Nincs hozzárendelt szerződés";
+
+$LANGPDF["software"][1]="Nincs telepített szoftver";
+?>
