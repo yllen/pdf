5cb6737b956c8248a5b472d18e133184ca287a2c
tsmr <tsmr@349b9182-4a13-0410-896f-e5e9767dd1b3>
2008-03-21 17:46:16 +0000

    [pdf] add pt_BR.php
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@43 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/pt_BR.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2005 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: BALPE DÃ©vi
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+$LANGPDF["title"][1]="Imprimir em PDF";
+$LANGPDF["title"][2]="Escolha os campos para imprimir";
+
+$LANGPDF["button"][1]="Imprimir";
+$LANGPDF["button"][2]="Salvar";
+
+$LANGPDF["config"][1]="Configuração do Plugin";
+$LANGPDF["config"][2]="Instalar o PDF Plugin";
+$LANGPDF["config"][3]="Remover o PDF Plugin";
+$LANGPDF["config"][4]="Configuração das suas Preferências";
+$LANGPDF["config"][5]="Tipo de Inventário :";
+$LANGPDF["config"][6]="Gerenciar direitos de Perfis";
+$LANGPDF["config"][7]="Direitos para este perfil";
+
+$LANGPDF["note"][1]="Nenhuma Nota Encontrada";
+
+$LANGPDF["document"][1]="Nenhum Documento Associado";
+
+$LANGPDF["financial"][1]="Nenhum informação Financeira";
+$LANGPDF["financial"][2]="Nenhum contrato associado";
+
+$LANGPDF["software"][1]="Nenhum software instalado";
+?>
