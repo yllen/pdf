85b6639f87855078838885420e623b423d76871c
walid <walid@349b9182-4a13-0410-896f-e5e9767dd1b3>
2008-05-30 12:49:12 +0000

    [pdf] Add polish locales (thanks to jacek102)
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@54 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/pl_PL.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2005 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+        This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: BALPE DĂŠvi
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+$LANGPDF["title"][1]="Drukuj do PDF";
+$LANGPDF["title"][2]="Wybierz tabele do wydruku PDF";
+
+$LANGPDF["button"][1]="Drukuj";
+$LANGPDF["button"][2]="Zapisz";
+
+$LANGPDF["config"][1]="Konfiguracja drukowania PDF";
+$LANGPDF["config"][2]="Instaluj wtyczke Drukuj do PDF";
+$LANGPDF["config"][3]="Odinstaluj wtyczke Drukuj do PDF";
+$LANGPDF["config"][4]="Konfiguracja";
+$LANGPDF["config"][5]="Typy w spisie:";
+$LANGPDF["config"][6]="Zarzadzanie prawami dostepu";
+$LANGPDF["config"][7]="Prawa do tego profilu";
+
+$LANGPDF["note"][1]="Nic nie znaleziono";
+
+$LANGPDF["document"][1]="Brak powiazanych dokumentow";
+
+$LANGPDF["financial"][1]="Brak informacji finansowych";
+$LANGPDF["financial"][2]="Brak powiazanych kontraktow";
+
+$LANGPDF["software"][1]="Brak zainstalowanego oprogramowania";
+?>
