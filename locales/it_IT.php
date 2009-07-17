827892c96a1c98aaf460f03c95d15221166367fc
remi <remi@349b9182-4a13-0410-896f-e5e9767dd1b3>
2009-07-17 18:26:11 +0000

    add italian translation - fixed #253
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@120 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/it_IT.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2008 by the INDEPNET Development Team.
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+// Original Author of file: BALPE Dévi
+// Purpose of file: plugin PDF - Italian traslation - Passero 17.07.2009
+// ----------------------------------------------------------------------
+
+$LANG['plugin_pdf']["title"][1]="Stampa pdf";
+$LANG['plugin_pdf']["title"][2]="Scelta tabelle da stampare";
+
+$LANG['plugin_pdf']["button"][1]="Stampa";
+$LANG['plugin_pdf']["button"][2]="Salva";
+
+$LANG['plugin_pdf']["config"][1]="Configurazione del plugin pdf";
+$LANG['plugin_pdf']["config"][6]="Gestione diritti dei profili";
+$LANG['plugin_pdf']["config"][7]="Diritti del profilo";
+$LANG['plugin_pdf']["config"][8]="http://www.glpi-project.org/spip.php?article248";
+
+$LANG['plugin_pdf']["note"][1]="Nessuna nota";
+
+$LANG['plugin_pdf']["document"][1]="Nessun documento associato";
+
+$LANG['plugin_pdf']["financial"][1]="Nessuna informazione finanziaria";
+$LANG['plugin_pdf']["financial"][2]="Nessun contratto associato";
+
+$LANG['plugin_pdf']["software"][1]="Software non installato";
+?>
