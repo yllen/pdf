5c0ca7fdf56285356a1d35d255c446a6ece2d4c0
remi <remi@349b9182-4a13-0410-896f-e5e9767dd1b3>
2009-07-20 19:04:44 +0000

    [pdf] Add turkish translation - fixed #259
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@121 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/tr_TR.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: Mehmet Kutlu inanc 
+// Purpose of file: Turkish translation
+// ----------------------------------------------------------------------
+
+$LANG['plugin_pdf']["title"][1]="Pdf olarak yazdır";
+$LANG['plugin_pdf']["title"][2]="PDF olarak yazdırmak için tablo seçiniz";
+
+$LANG['plugin_pdf']["button"][1]="Yazdır";
+$LANG['plugin_pdf']["button"][2]="Kaydet";
+
+$LANG['plugin_pdf']["config"][1]="Pdf eklentisi yapılandırması";
+$LANG['plugin_pdf']["config"][6]="Profil yetki yönetimi";
+$LANG['plugin_pdf']["config"][7]="Profil yetkileri";
+$LANG['plugin_pdf']["config"][8]="http://www.glpi-project.org/spip.php?article248";
+
+$LANG['plugin_pdf']["note"][1]="Not bulunamadı";
+
+$LANG['plugin_pdf']["document"][1]="İlgili döküman bulunamadı";
+
+$LANG['plugin_pdf']["financial"][1]="Finansal bilgi bulunamadı";
+$LANG['plugin_pdf']["financial"][2]="İlgili sözleşme bulunamadı";
+
+$LANG['plugin_pdf']["software"][1]="Kurulu yazılım bulunamadı";
+?>
