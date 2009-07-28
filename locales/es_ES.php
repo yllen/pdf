3b9b247a12bbeead14e096cc9f7689ffa83db154
remi <remi@349b9182-4a13-0410-896f-e5e9767dd1b3>
2009-07-28 19:23:04 +0000

    [pdf] es_ES translation - fixed #283
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@125 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/es_ES.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: francisco f.
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+$LANG['plugin_pdf']["title"][1]="Impresión a pdf";
+$LANG['plugin_pdf']["title"][2]="Elegir las tablas a imprimir a pdf";
+
+$LANG['plugin_pdf']["button"][1]="Imprimir";
+$LANG['plugin_pdf']["button"][2]="Guardar";
+
+$LANG['plugin_pdf']["config"][1]="Configuración del plugin de Impresión a pdf";
+$LANG['plugin_pdf']["config"][6]="Gestión de los permisos por perfil";
+$LANG['plugin_pdf']["config"][7]="Permisos para el perfil";
+$LANG['plugin_pdf']["config"][8]="http://www.glpi-project.org/spip.php?article229";
+
+$LANG['plugin_pdf']["note"][1]="Ninguna nota encontrada";
+
+$LANG['plugin_pdf']["document"][1]="Ningún documento asociado";
+
+$LANG['plugin_pdf']["financial"][1]="Ninguna información financiera";
+$LANG['plugin_pdf']["financial"][2]="Ningún contrato asociado";
+
+$LANG['plugin_pdf']["software"][1]="Ningún programa instalado";
+?>
