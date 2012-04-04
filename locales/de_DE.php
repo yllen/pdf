aebcbdda0d30fe6556296d4651b2b4ebb5e2a523
remi <remi@349b9182-4a13-0410-896f-e5e9767dd1b3>
2012-04-04 06:40:48 +0000

    add German translation, fixed #3442
    git-svn-id: https://forge.glpi-project.org/svn/pdf/trunk@319 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/de_DE.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ pdf - Export to PDF plugin for GLPI
+ Copyright (C) 2003-2012 by the pdf Development Team.
+
+ https://forge.indepnet.net/projects/pdf
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of pdf.
+
+ pdf is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ pdf is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with pdf. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+// Original Author of file: Mathieu Simon
+// Purpose of file: German localization
+// ----------------------------------------------------------------------
+
+$LANG['plugin_pdf']['title'][1] = "PDF-Ausgabe";
+$LANG['plugin_pdf']['title'][2] = "Kategorien für PDF-Ausgabe wählen";
+
+$LANG['plugin_pdf']['button'][1] = "Drucken";
+$LANG['plugin_pdf']['button'][2] = "Speichern";
+
+$LANG['plugin_pdf']['group'][1] = "Material von Mitgliedern";
+
+$LANG['plugin_pdf']['knowbase'][1] = "Keine Fragen gefunden";
+$LANG['plugin_pdf']['knowbase'][2] = "Keine Antworten gefunden";
+
+$LANG['plugin_pdf']['note'][1] = "Keine Notiz gefunden";
+
+$LANG['plugin_pdf']['document'][1] = "Keine verknüpften Dokumente";
+
+$LANG['plugin_pdf']['financial'][1] = "Keine Buchhaltungsdaten";
+$LANG['plugin_pdf']['financial'][2] = "Keine verknüpften Verträge";
+
+$LANG['plugin_pdf']['software'][1] = "Keine installierte Software";
+
+
+$LANG['plugin_pdf']['ticket'][1] = "Anmerkungen";
+$LANG['plugin_pdf']['ticket'][2] = "Aufgaben";
+$LANG['plugin_pdf']['ticket'][3] = "Alte Ticket(s)";
+$LANG['plugin_pdf']['ticket'][4] = "Kein altes Ticket";
+$LANG['plugin_pdf']['ticket'][5] = "Keine Antwort auf diese Anfrage";
