24332c3a05212ce69e897ea26905cbdf449abb4d
yllen <nini.lasson@orange.fr>
2022-06-14 16:32:32 +0200

    fixed #15 - display solution

--- inc/itilsolution.class.php
+++ inc/itilsolution.class.php
@@ -21,7 +21,7 @@
 
  @package   pdf
  @authors   Nelly Mahu-Lasson, Remi Collet
- @copyright Copyright (c) 2018-2020 PDF plugin team
+ @copyright Copyright (c) 2018-2022 PDF plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link      https://forge.glpi-project.org/projects/pdf
@@ -68,9 +68,8 @@ class PluginPdfITILSolution extends PluginPdfCommon {
             } else {
                $title = __('Solution');
             }
-            $sol = Html::clean(Toolbox::unclean_cross_side_scripting_deep(
-                                                      html_entity_decode($row['content'],
-                                                                         ENT_QUOTES, "UTF-8")));
+            $sol = Toolbox::unclean_cross_side_scripting_deep(html_entity_decode($row['content'],
+                                                                                 ENT_QUOTES, "UTF-8"));
 
             if ($row['status'] == 3) {
                $text = __('Soluce approved on ', 'pdf');
