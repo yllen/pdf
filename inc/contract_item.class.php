515f6eb82645682d493e48048f24c4ea28ba552d
yllen <nini.lasson@orange.fr>
2022-06-14 15:50:50 +0200

    fixed #3 - items list in contract

--- inc/contract_item.class.php
+++ inc/contract_item.class.php
@@ -20,7 +20,7 @@
 
  @package   pdf
  @authors   Nelly Mahu-Lasson, Remi Collet
- @copyright Copyright (c) 2009-2021 PDF plugin team
+ @copyright Copyright (c) 2009-2022 PDF plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link      https://forge.glpi-project.org/projects/pdf
@@ -176,23 +176,7 @@ class PluginPdfContract_Item extends PluginPdfCommon {
             $iterator = $DB->request($params);
             $nb       = count($iterator);
 
-            if ($nb > $_SESSION['glpilist_limit']) {
-               $opt = ['order'      => 'ASC',
-                       'is_deleted' => 0,
-                       'reset'      => 'reset',
-                       'start'      => 0,
-                       'sort'       => 80,
-                       'criteria'   => [0 => ['value'      => '$$$$'.$instID,
-                                              'searchtype' => 'contains',
-                                              'field'      => 29]]];
-
-               $link = ". __('Device list').";
-
-               $data[$itemtype] = ['longlist' => true,
-                                   'name'     => sprintf(__('%1$s: %2$s'),
-                                                         $item->getTypeName($nb), $nb),
-                                   'link'     => $link];
-            } else if ($nb > 0) {
+            if ($nb > 0) {
                $data[$itemtype] = [];
                while ($objdata = $iterator->next()) {
                   $data[$itemtype][$objdata['id']] = $objdata;
