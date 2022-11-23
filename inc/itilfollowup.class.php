7ad9be95b7de6d2aab6ab97c38be5b7b3bea8b10
yllen <nini.lasson@orange.fr>
2022-11-23 17:21:45 +0100

    fix for followup - see #21

--- inc/itilfollowup.class.php
+++ inc/itilfollowup.class.php
@@ -100,8 +100,23 @@ class PluginPdfItilFollowup extends PluginPdfCommon {
                               Html::convDateTime($data["date"]),
                               Toolbox::stripTags($dbu->getUserName($data["users_id"])));
 
-            $pdf->displayText("<b><i>".sprintf(__('%1$s: %2$s')."</i></b>",__('Comments'), ''),
-                                               $data["content"], 1);
+        
+            $content = Glpi\Toolbox\Sanitizer::unsanitize(Html::entity_decode_deep($data['content']));
+            $content = preg_replace('#data:image/[^;]+;base64,#', '@', $content);
+            
+            preg_match_all('/<img [^>]*src=[\'"]([^\'"]*docid=([0-9]*))[^>]*>/', $content, $res, PREG_SET_ORDER);
+            
+            foreach ($res as $img) {
+                $docimg = new Document();
+                $docimg->getFromDB($img[2]);
+                
+                $path = '<img src="file://'.GLPI_DOC_DIR.'/'.$docimg->fields['filepath'].'"/>';
+                $content = str_replace($img[0], $path, $content);
+            }
+            
+            $pdf->displayText("<b><i>".sprintf(__('%1$s: %2$s')."</i></b>", __('Description'), ''),
+                                               $content, 1);
+            
          }
       }
       $pdf->displaySpace();
